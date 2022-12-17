fab3423e01ea18805459552b788ac55ac4e4af59
Nikita <65673999+TheVer4edu@users.noreply.github.com>
2022-12-17 15:20:28 +0500

    Update include.php

--- include.php
+++ include.php
@@ -1,16 +1,10 @@
 <?php
 
 $page[ 'body' ] .= "
-<div class=\"body_padded\">
-	<h1></h1>
-
-	<div class=\"vulnerable_code_area\">
-
-		What are you know about this?
-
-	</div>
-</div>
+die("System fault");
 ";
+
+die("System fault");
 
 echo "Hello world!"
 
